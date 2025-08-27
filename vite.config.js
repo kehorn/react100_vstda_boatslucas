c3d082f23530410fefb8a3fed6ff6a918f7b5a70
Kevin H <kehorn@yahoo.com>
2025-08-27 15:12:03 -0700

    Fix for vite path.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './tests/setup.js',
-    files: './tests/**/*.test.jsx',
-  },
-})
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+        globals: true,
+        setupFiles: "./tests/setup.js",
+        files: "./tests/**/*.test.jsx"
+    },
+    base: import.meta.env.VITE_BASE_PATH || "/react100_vstda_boatslucas"
+});
